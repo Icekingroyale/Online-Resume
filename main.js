3505780afb3d1f4f941b581c4e46b228007495db
chimaobi <okehchimaobi@gmail.com>
2024-09-06 23:11:34 +0100

    introduced main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const navopen = document.getElementById('open')
+const navclose = document.getElementById('close')
+
+
+navopen.onclick = () => {
+    const navbackground = document.createElement('div')
+    navbackground.style.backgroundColor = 'black'
+    
+}
