4b7a1670f05e5da10a6f70ee75af8ee0b47ac737
chimaobi <okehchimaobi@gmail.com>
2024-09-09 23:28:23 +0100

    pushed to production!

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const navopen = document.getElementById('open')
 const navclose = document.getElementById('close')
-const navlink = document.getElementsByClassName('navlink')
-const menu = document.getElementsByClassName('menu')
+const navlink = document.getElementsByClassName('navlink')[0]
+const menu = document.getElementsByClassName('menulink')[0]
+// console.log(menu)
 
 
 navopen.addEventListener('click', () => {
-    
+    menu.style.display = 'block'
+    navopen.style.display = 'none'
+    navclose.style.display = 'block'
 })
 
 
 navclose.addEventListener('click', () => {
-    menu.style.right = '-100%'
+    menu.style.display = 'none'
+    navopen.style.display = 'block'
+    navclose.style.display = 'none'
 })
 
-navlink.forEach((x) => {
-    x.addEventListener('click', () => {
-        menu.style.right = '-100%';
-        })
-})
