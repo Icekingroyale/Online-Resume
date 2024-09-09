800484d8411b540021fe07bca4bff15c74f005ae
chimaobi <okehchimaobi@gmail.com>
2024-09-09 22:50:37 +0100

    update

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const navopen = document.getElementById('open')
 const navclose = document.getElementById('close')
+const navlink = document.getElementsByClassName('navlink')
+const menu = document.getElementsByClassName('menu')
 
 
-navopen.onclick = () => {
-    const navbackground = document.createElement('div')
-    navbackground.style.backgroundColor = 'black'
+navopen.addEventListener('click', () => {
     
-}
+})
+
+
+navclose.addEventListener('click', () => {
+    menu.style.right = '-100%'
+})
+
+navlink.forEach((x) => {
+    x.addEventListener('click', () => {
+        menu.style.right = '-100%';
+        })
+})
